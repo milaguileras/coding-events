45b95fc9b06cf14fcdd39b21a017acb61bf4b236
Camila <camila@Matthews-MacBook-Pro-2.local>
2021-03-04 13:40:21 -0600

    mm

--- src/main/java/org/launchcode/codingevents/models/Event.java
+++ src/main/java/org/launchcode/codingevents/models/Event.java
@@ -1,2 +1,32 @@
-package org.launchcode.codingevents.models;public class Event {
+package org.launchcode.codingevents.models;
+
+public class Event {
+
+    private String name;
+    private String description;
+
+    public Event(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription(){
+        return description;
+    }
+
+    public void setDescription(String description){
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
 }
