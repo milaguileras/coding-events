71972373f29a4290a1fbecb75049524d1abf6123
Camila <camila@Matthews-MacBook-Pro-2.local>
2021-03-17 16:38:05 -0500

    .

--- src/main/java/org/launchcode/codingevents/models/Event.java
+++ src/main/java/org/launchcode/codingevents/models/Event.java
@@ -1,5 +1,8 @@
 package org.launchcode.codingevents.models;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.util.Objects;
 
 /**
@@ -10,14 +13,27 @@ public class Event {
     private int id;
     private static int nextId = 1;
 
+    @NotBlank
+    @Size(min = 3, max = 50, message = "Name should be between 3 and 50 characters")
     private String name;
+
+    @Size(max = 500, message = "Description too long")
     private String description;
 
-    public Event(String name, String description) {
+    @NotBlank
+    @Email (message = "Email not valid, try again" )
+    private String contactEmail;
+
+    public Event(String name, String description, String contactEmail) {
         this.name = name;
         this.description = description;
+        this.contactEmail = contactEmail;
         this.id = nextId;
         nextId++;
+    }
+
+    public Event(){
+
     }
 
     public String getName() {
@@ -34,6 +50,14 @@ public class Event {
 
     public void setDescription(String description) {
         this.description = description;
+    }
+
+    public String getContactEmail() {
+        return contactEmail;
+    }
+
+    public void setContactEmail(String contactEmail) {
+        this.contactEmail = contactEmail;
     }
 
     public int getId() {
